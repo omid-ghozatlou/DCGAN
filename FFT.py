00f7d030b971f59c1086a75869ae961cc1ec87bc
Omid Ghozatlou <46296636+omid-ghozatlou@users.noreply.github.com>
2023-05-04 11:20:58 +0300

    Update FFT.py

--- FFT.py
+++ FFT.py
@@ -86,4 +86,89 @@ class Generator(nn.Module):
 
     def forward(self, z):
         return self.fc(z)
+#Define the discriminator network
+class Discriminator(nn.Module):
+    def init(self):
+        super(Discriminator, self).init()
+        self.fc = nn.Sequential(
+            nn.Linear(784, 512),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Linear(512, 256),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Linear(256, 1),
+            nn.Sigmoid()
+        )
 
+    def forward(self, x):
+        x = x.view(x.size(0), -1)
+        return self.fc(x)
+#Define the hyperparameters
+batch_size = 128
+lr = 0.0002
+latent_dim = 100
+num_epochs = 200
+
+#Create the dataset and data loader
+dataset = FourierTransformDataset(root='./data')
+dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers=4)
+
+#Initialize the generator and discriminator networks
+generator = Generator(latent_dim)
+discriminator = Discriminator()
+
+#Define the loss function and optimizer
+criterion = nn.BCELoss()
+generator_optimizer = optim.Adam(generator.parameters(), lr=lr)
+discriminator_optimizer = optim.Adam(discriminator.parameters(), lr=lr)
+
+#Define the device (CPU or GPU) to use for training
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+#Move the generator and discriminator networks to the device
+generator = generator.to(device)
+discriminator = discriminator.to(device)
+
+#Train the GAN
+for epoch in range(num_epochs):
+    for i, data in enumerate(dataloader):
+
+        # Train the discriminator
+        real_images = data.to(device)
+        real_labels = torch.ones(batch_size, 1).to(device)
+        fake_labels = torch.zeros(batch_size, 1).to(device)
+
+        # Generate fake images
+        z = torch.randn(batch_size, latent_dim).to(device)
+        fake_images = generator(z)
+
+        # Train the discriminator with real and fake images
+        discriminator_real_outputs = discriminator(real_images)
+        discriminator_fake_outputs = discriminator(fake_images.detach())
+        discriminator_real_loss = criterion(discriminator_real_outputs, real_labels)
+        discriminator_fake_loss = criterion(discriminator_fake_outputs, fake_labels)
+        discriminator_loss = discriminator_real_loss + discriminator_fake_loss
+
+        discriminator_optimizer.zero_grad()
+        discriminator_loss.backward()
+        discriminator_optimizer.step()
+
+        # Train the generator
+        z = torch.randn(batch_size, latent_dim).to(device)
+        fake_images = generator(z)
+        generator_outputs = discriminator(fake_images)
+        generator_loss = criterion(generator_outputs, real_labels)
+
+        generator_optimizer.zero_grad()
+        generator_loss.backward()
+        generator_optimizer.step()
+
+        # Print the loss values every 100 iterations
+        if (i+1) % 100 == 0:
+            print(f"Epoch [{epoch+1}/{num_epochs}], Step [{i+1}/{len(dataloader)}], Discriminator Loss: {discriminator_loss.item():.4f}, Generator Loss: {generator_loss.item():.4f}")
+
+    # Save the generated images every 3 epochs
+    if (epoch+1) % 3 == 0:
+        with torch.no_grad():
+            z = torch.randn(64, latent_dim).to(device)
+            fake_images = generator(z).reshape(-1, 3, 256, 256)
+            save_image(fake_images, f'generated_images_{epoch+1}.png', nrow=8, normalize=True)
